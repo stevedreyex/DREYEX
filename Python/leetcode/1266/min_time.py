1a9431c722300cc7ead7ea9fe10655e47bc51fff
廖子濬 <dreyex@liaozijunde-MacBook-Pro.local>
2019-11-30 12:52:39 +0800

    start to leetcode

---
+++ Python/leetcode/1266/min_time.py
@@ -0,0 +1,3 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        return sum(max(abs(points[i][0] - points[i-1][0]), abs(points[i][1] - points[i-1][1])) for i in range(1, len(points)))
