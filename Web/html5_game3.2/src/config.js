c4db012b151d09fb6219892f2d92db8a757ebde7
Stefan Liao <t107820005@ntut.org.tw>
2020-07-30 12:41:30 +0800

    ooplab html5 framework

---
+++ Web/html5_game3.2/src/config.js
@@ -0,0 +1,16 @@
+// By Raccoon
+// include namespace
+var Framework = (function (Framework) {
+	'use strict'
+	Framework.Config = function () {
+		this.fps = 60;
+		this.canvasWidth = 1350;
+		this.canvasHeight = 700;
+//		this.canvasWidth =  640;
+//		this.canvasHeight = 480;
+		this.isBackwardCompatiable = false;
+		this.isOptimize = false;  // 2017.02.20, from V3.1.1
+		this.isMouseMoveRecorded = false;
+	};
+	return Framework;
+})(Framework || {});
---
+++ Web/javascripts/code.js
@@ -0,0 +1,5 @@
+alert("code.js is using");
+
+// document.write("<h1>Hello world</h1>");
+// document.write("<hr>");
+// document.write("<p>This is a javascript tutorial</p>");
